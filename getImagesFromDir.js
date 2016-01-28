105b7c9d8fa75984ae08bd4f389dfe42d707601d
zc1415926 <zc1415926@126.com>
2016-01-28 16:17:30 +0800

    bugfix: 修正图片没有缩放的问题

--- getImagesFromDir.js
+++ getImagesFromDir.js
@@ -5,16 +5,19 @@
 var shell = require("shelljs");
 var xlsx = require('node-xlsx');
 var images = require('images');
+var progress = require('progress');
 
 var xlsxFileName = process.argv[2];
 var dirPath = process.argv[3];
 var extension;
 
 var sourceData = xlsx.parse(xlsxFileName)[0]['data'];
-
+var progressBar = new progress('Converting: [:bar] :percent', {
+    incomplete: ' ',
+    total: sourceData.length});
 
 shell.cd(dirPath);
-
+var noPhotoArray = [];
 for(i = 1; i < sourceData.length; i++)
 {
     if(shell.ls(sourceData[i][4]+"*").length >0)
@@ -22,11 +25,19 @@ for(i = 1; i < sourceData.length; i++)
         extension = shell.ls(sourceData[i][4]+"*")[0].split('.')[1];
 
         //先缩放图片再移动图片效率高些
-        images(sourceData[i][4] + "." + extension).size(120);
+        images(sourceData[i][4] + "." + extension).resize(120).save(sourceData[i][4] + "." + extension);
         shell.mv('-f', sourceData[i][4] + "." + extension, sourceData[i][0] + "." + extension );
     }
     else
     {
-        console.log("缺少照片！\t" + sourceData[i][4]);
+        //console.log("缺少照片！\t" + sourceData[i][4]);
+        noPhotoArray.push(sourceData[i][4]);
     }
+
+    progressBar.tick();
 }
+progressBar.tick(progressBar.total - progressBar.curr);
+console.log('\n');
+noPhotoArray.forEach(function(data){
+    console.log("缺少照片！\t" + data);
+});
